bc09b898f638c41848b5ed38e7c7e7fb7a0860c1
“Ahsanamjad967git config --global user.name “Ahsanamjad967 <studentpaystudent@gmail.com>
2024-08-24 23:57:46 +0500

    first commit

---
+++ server.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const http = require('http');
+const { Server } = require('socket.io');
+const messageRoutes = require('./routes/message');
+require('dotenv').config();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:3000",  // React frontend URL
+    methods: ["GET", "POST"],
+  },
+});
+
+// Connect to MongoDB
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+.then(() => console.log('MongoDB connected'))
+.catch(err => console.error('MongoDB connection error:', err));
+
+app.use('/messages', messageRoutes);
+// Socket.io logic
+io.on('connection', (socket) => {
+  console.log('New user connected:', socket.id);
+
+  socket.on('sendMessage', (message) => {
+    io.emit('receiveMessage', message);
+  });
+
+  socket.on('disconnect', () => {
+    console.log('User disconnected:', socket.id);
+  });
+});
+
+// Start the server
+server.listen(process.env.PORT, () => {
+  console.log(`Server running on http://localhost:${process.env.PORT}`);
+});
