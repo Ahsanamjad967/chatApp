61f3ac3fe8d96b8211f075739e37cedeefd79ef1
Ahsanamjad967 <81257282+Ahsanamjad967@users.noreply.github.com>
2024-08-25 16:00:57 +0500

    Update server.js

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.use(express.json());
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:3000",  // React frontend URL
+    origin: "https://ahsanchatapp.netlify.app",  // React frontend URL
     methods: ["GET", "POST"],
   },
 });
